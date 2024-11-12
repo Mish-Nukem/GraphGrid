7f8e19d5463fe1b6ecf19630ae18671f2b3d4e7f
Mish_Nukem <mr@it-tech.email>
2024-11-12 18:56:30 +0300

    window options changes

--- GraphGrid/GraphGridJS/wwwroot/js/Modals.js
+++ GraphGrid/GraphGridJS/wwwroot/js/Modals.js
@@ -5,55 +5,35 @@ export default class Modal {
 
         this.opt = options || {};
 
-        this.zInd = options.zInd || 999;
+        this.opt.zInd = this.opt.zInd || 999;
 
-        window._wndDict = window._wndDict || { seq: 0, lastZInd: this.zInd };
+        window._wndDict = window._wndDict || { seq: 0, lastZInd: this.opt.zInd };
 
         this.id = window._wndDict.seq++;
-        //this.id =  `window_${}_`;
         window._wndDict[this.id] = this;
-        //window._wndDict.seq++;
 
-        this.title = options.title;
-
-        this.isOverlay = options.isOverlay || false;
-
-        this.isHidden = options.isHidden || false;
-
-        this.isModal = options.isModal || false;
-
-        this.pos = options.pos || { x: 0, y: 0, w: '100%', h: '100%' };
+        this.opt.pos = this.opt.pos || { x: 0, y: 0, w: '100%', h: '100%' };
 
         this.utils = new Utils();
-
-        this.closeWhenClick = options.closeWhenClick || false;
-
-        this.closeWhenMiss = options.closeWhenMiss || false;
-
-        this.style = options.style || '';
 
         this.drawBody = options.drawBody;
 
         this.drawHeader = options.drawHeader !== undefined ? options.drawHeader : this.drawHeader;
 
         this.drawFooter = options.drawFooter !== undefined ? options.drawFooter : this.drawFooter; 
-
-        this.windowClass = options.windowClass;
-
-        this.resizable = options.resizable;
     }
 
     show = function () {
-        if (!this.isOverlay && (this.isModal || this.closeWhenMiss || this.owner)) {
+        if (!this.opt.isOverlay && (this.opt.isModal || this.opt.closeWhenMiss || this.owner)) {
             this.overlay = new Modal({
-                zInd: this.zInd - 1,
+                zInd: this.opt.zInd - 1,
                 isOverlay: true,
                 drawHeader: false,
                 drawFooter: false,
-                closeWhenClick: !this.isModal,
+                closeWhenClick: !this.opt.isModal,
                 resizable: false,
                 pos: { x: 0, y: 0, w: '100%', h: '100%' },
-                isHidden: this.closeWhenMiss,
+                isHidden: this.opt.closeWhenMiss,
             });
 
             this.overlay.owner = this;
@@ -61,15 +41,15 @@ export default class Modal {
             this.overlay.show();
         }
 
-        let x = this.utils.isInt(this.pos.x) ? this.pos.x + 'px' : this.pos.x;
-        let y = this.utils.isInt(this.pos.y) ? this.pos.y + 'px' : this.pos.y;
-        let w = this.utils.isInt(this.pos.w) ? this.pos.w + 'px' : this.pos.w;
-        let h = this.utils.isInt(this.pos.h) ? this.pos.h + 'px' : this.pos.h;
+        let x = this.utils.isInt(this.opt.pos.x) ? this.opt.pos.x + 'px' : this.opt.pos.x;
+        let y = this.utils.isInt(this.opt.pos.y) ? this.opt.pos.y + 'px' : this.opt.pos.y;
+        let w = this.utils.isInt(this.opt.pos.w) ? this.opt.pos.w + 'px' : this.opt.pos.w;
+        let h = this.utils.isInt(this.opt.pos.h) ? this.opt.pos.h + 'px' : this.opt.pos.h;
 
-        let opacity = this.isOverlay ? this.isHidden ? 'opacity: 0;' : 'opacity: 0.2;' : this.opacity ? `opacity: ${this.opacity};` : '';
-        let backgroundColor = this.isOverlay && !this.isHidden ? 'background:black;' : '';
-        let style = this.style;
-        let windowClass = this.windowClass || '';
+        let opacity = this.opt.isOverlay ? this.opt.isHidden ? 'opacity: 0;' : 'opacity: 0.2;' : this.opt.opacity ? `opacity: ${this.opt.opacity};` : '';
+        let backgroundColor = this.opt.isOverlay && !this.opt.isHidden ? 'background:black;' : '';
+        let style = this.opt.style || '';
+        let windowClass = this.opt.windowClass || '';
 
         let div = document.createElement('div');
 
@@ -79,7 +59,7 @@ export default class Modal {
                 left: ${x};
                 width: ${w};
                 height: ${h};
-                z-index: ${this.zInd};
+                z-index: ${this.opt.zInd};
                 position: fixed;
                 ${opacity}
                 ${backgroundColor}
@@ -97,15 +77,15 @@ export default class Modal {
             s += this.drawFooter();
         }
 
-        if (!this.isOverlay && this.resizable != false) {
+        if (!this.opt.isOverlay && this.opt.resizable != false) {
             s += `<div wnd-rsz="${this.id}-y"
-                style="position: absolute;left: -1px;bottom: -6px;cursor: s-resize;height: 12px;width: ${this.pos.w - 10}px;z-index: ${this.zInd + 5};">
+                style="position: absolute;left: -1px;bottom: -6px;cursor: s-resize;height: 12px;width: ${this.opt.pos.w - 10}px;z-index: ${this.opt.zInd + 5};">
                 </div>
             <div wnd-rsz="${this.id}-x"
-                style="position: absolute;right: -6px;top: -1px;cursor: e-resize;height: ${this.pos.h - 10}px;width: 12px;z-index: ${this.zInd + 5};">
+                style="position: absolute;right: -6px;top: -1px;cursor: e-resize;height: ${this.opt.pos.h - 10}px;width: 12px;z-index: ${this.opt.zInd + 5};">
                 </div>
             <div wnd-rsz="${this.id}-xy"
-                style="position: absolute;right: -5px;bottom: -5px;cursor: se-resize;height: 16px;width: 16px;z-index: ${this.zInd + 5};">
+                style="position: absolute;right: -5px;bottom: -5px;cursor: se-resize;height: 16px;width: 16px;z-index: ${this.opt.zInd + 5};">
                 </div>`;
         }
 
@@ -118,7 +98,7 @@ export default class Modal {
     }
 
     drawHeader = function () {
-        let headerClass = this.headerClass || '';
+        let headerClass = this.opt.headerClass || '';
 
         return `<div class="${headerClass}" style="display: flex;flex-wrap: nowrap;justify-content: space-between;align-items: center;">
         <h4>${this.opt.title || ''}</h4>
@@ -156,7 +136,7 @@ document.addEventListener('click', function (e) {
 
     switch (parts[0]) {
         case 'window':
-            if (wnd.closeWhenClick || wnd.owner && wnd.owner.closeWhenMiss) {
+            if (wnd.opt.closeWhenClick || wnd.owner && wnd.owner.opt.closeWhenMiss) {
                 if (wnd.overlay) {
                     wnd.overlay.close();
                 }
