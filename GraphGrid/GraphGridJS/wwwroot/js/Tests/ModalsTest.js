5b49786538f980b5e903673a4909f1247340ecbd
Mish_Nukem <mr@it-tech.email>
2024-11-12 18:32:06 +0300

    extend Modals

--- GraphGrid/GraphGridJS/wwwroot/js/Tests/ModalsTest.js
+++ GraphGrid/GraphGridJS/wwwroot/js/Tests/ModalsTest.js
@@ -1,7 +1,50 @@
 ﻿import Modal from '../Modals.js';
 
-export function TestShow() {
+export function TestOverlay() {
 
-    let wnd = new Modal('Test Window');
+    let wnd = new Modal({
+        isOverlay: true,
+        closeWhenClick: true,
+    });
+    wnd.show();
+}
+
+export function TestPopupWnd() {
+    let wnd = new Modal({
+        closeWhenClick: true,
+        closeWhenMiss: true,
+        resizable: false,
+        drawHeader: false,
+        drawFooter: false,
+        pos: {
+            x: 120,
+            y: 120,
+            w: 200,
+            h: 200
+        },
+        style: 'background:white;border:1px solid;',
+        drawBody: function () {
+            return 'Test popup content';
+        }
+    });
+    wnd.show();
+}
+
+export function TestModalWnd() {
+    let wnd = new Modal({
+        //closeWhenClick: true,
+        isModal: true,
+        title: 'Test modal title',
+        pos: {
+            x: 120,
+            y: 120,
+            w: 200,
+            h: 200
+        },
+        style: 'background:white;border:1px solid;',
+        drawBody: function () {
+            return 'Test modal content';
+        }
+    });
     wnd.show();
 }
---
+++ GraphGrid/GraphGridJS/wwwroot/js/Utils.js
@@ -0,0 +1,52 @@
+﻿export default class Utils {
+    constructor() {
+    }
+
+    isInt = function (n) {
+        return isNaN(n) ? !1 : n % 1 === 0;
+    }
+
+    getMousePos = function (pageX, pageY, ctrl, noOffset) {
+        pageX = pageX || 0;
+        pageY = pageY || 0;
+
+        let rect = { left: 0, top: 0 };
+        if (ctrlId) {
+            let elem = typeof ctrl == 'object' ? ctrl : document.getElementById(ctrl);
+            rect = elem ? elem.getBoundingClientRect() : rect;
+        }
+
+        let res = { x: pageX - rect.left - (noOffset ? 0 : window.scrollX), y: pageY - rect.top - (noOffset ? 0 : window.scrollY) };
+
+        //if (ctrlId) {
+        //    let par = ctrl;
+        //    while (par.length) {
+        //        if (par.prop('innerHTML') && par.prop('tagName') != 'HTML') {
+        //            res.x += par.scrollLeft();
+        //            res.y += par.scrollTop();
+        //        }
+        //        par = par.parent();
+        //    }
+        //}
+
+        return res;
+    }
+
+    getControlPos = function (ctrl) {
+
+        let elem = typeof ctrl == 'object' ? ctrl : document.getElementById(ctrl);
+        let box = elem.getBoundingClientRect();
+
+        return {
+            x: box.left + window.scrollX,
+            y: box.top + window.scrollY,
+            w: box.width,
+            h: box.height
+        }
+    }
+
+    //document.body.appendChild
+
+    // -------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+}
