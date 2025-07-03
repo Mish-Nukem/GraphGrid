ad260525b8c81cf75858829135c143fe3d1f769e
Mish_Nukem <mr@it-tech.email>
2025-07-03 16:29:55 +0300

    fix path

--- ReactGrid/vite.config.js
+++ ReactGrid/vite.config.js
@@ -4,7 +4,7 @@ import plugin from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [plugin()],
-    base: '/GraphGrid/tree/master/ReactGrid/',
+    base: '/ReactGrid/',
     server: {
         port: 61020,
     }
